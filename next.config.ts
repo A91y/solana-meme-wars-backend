08464d5955cd750641c6947d5aa56aaf5b3d3e49
Sudheer <108649080+sudheer0071@users.noreply.github.com>
2025-01-12 01:30:28 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://solana-meme-wars-backend.vercel.app/:path*',
+          },
+        ]
+      },
 };
 
 export default nextConfig;
