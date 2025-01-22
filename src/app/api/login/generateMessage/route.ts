418b9764f195e60641078005ca09cf491e954dca
A91y <ayush.agr254@gmail.com>
2025-01-22 21:55:28 +0530

    Update CORS headers to include Authorization for login API routes

--- src/app/api/login/generateMessage/route.ts
+++ src/app/api/login/generateMessage/route.ts
@@ -3,27 +3,27 @@ import { generateNonce } from "@/utils/solana";
 import { NextResponse } from "next/server";
 
 const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 export async function POST(req: Request) {
-    const { wallet } = await req.json();
-    try {
-        const message = CHALLENGE_PREFIX + generateNonce(wallet);
-        return NextResponse.json(message, { headers: corsHeaders });
-    } catch (error) {
-        return NextResponse.json(
-            { error: "Failed to toggle upvote" },
-            { status: 500, headers: corsHeaders }
-        );
-    }
+  const { wallet } = await req.json();
+  try {
+    const message = CHALLENGE_PREFIX + generateNonce(wallet);
+    return NextResponse.json(message, { headers: corsHeaders });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to toggle upvote" },
+      { status: 500, headers: corsHeaders }
+    );
+  }
 }
 
 export async function GET(req: Request) {
-    return NextResponse.json(
-        { error: "Method Not Allowed" },
-        { status: 405, headers: corsHeaders }
-    );
+  return NextResponse.json(
+    { error: "Method Not Allowed" },
+    { status: 405, headers: corsHeaders }
+  );
 }
