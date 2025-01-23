6d9c1c1ca57095f3f07adbef2a4f97d022b12e8f
A91y <ayush.agr254@gmail.com>
2025-01-23 21:58:25 +0530

    Fix AWS credentials environment variable names in image upload API

--- src/app/api/uploadImage/route.ts
+++ src/app/api/uploadImage/route.ts
@@ -68,8 +68,8 @@ export async function POST(req: NextRequest) {
     const awsClient = new S3Client({
       region: process.env.AWS_REGION,
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY ?? "",
-        secretAccessKey: process.env.AWS_SECRET_KEY ?? "",
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+        secretAccessKey: process.env.AWS_SECRET_KEY_ID ?? "",
       },
     });
     const bucket = process.env.AWS_BUCKET_NAME || "";
